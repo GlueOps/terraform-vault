94f7b78502e992dd4aa676971df06ad4dbb626a6
Venkata Mutyala <venkata.mutyala@glueops.dev>
2022-10-16 20:12:54 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -215,31 +215,4 @@ resource "vault_mount" "secrets_kvv2" {
   type        = "kv-v2"
   description = "KV Version 2 secrets mount"
 }
-  
-resource "tfe_organization" "test" {
-  name  = "my-org-name"
-  email = "admin@company.com"
-}
-
-resource "tfe_variable_set" "test" {
-  name         = "Test Varset"
-  description  = "Some description."
-  global       = false
-  organization = tfe_organization.test.name
-}
-
-resource "tfe_variable" "test-a" {
-  key             = "seperate_variable"
-  value           = "my_value_name"
-  category        = "terraform"
-  description     = "a useful description"
-  variable_set_id = tfe_variable_set.test.id
-}
-
-resource "tfe_variable" "test-b" {
-  key             = "another_variable"
-  value           = "my_value_name"
-  category        = "env"
-  description     = "an environment variable"
-  variable_set_id = tfe_variable_set.test.id
-}
+ 
